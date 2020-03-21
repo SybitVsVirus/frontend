ae620a14fbeb7a89e55d3e3ff710734986c48e79
Anton Schuster <mail2toni@gmail.com>
2020-03-21 23:31:58 +0100

    Update Register.vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="register">
       <v-container>
-        <v-row class="text-left">
+        <v-row>
             <v-col cols="12"><h1>Komm in unser Team</h1></v-col>
         </v-row>
-        <v-row class="text-left">
+        <v-row>
             <v-col cols="8">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-col>
         </v-row>
         <v-row>
