85a3b8e03652f76b5f064f7436470a5ff1723bbe
Anton Schuster <Anton.Schuster@sybit.de>
2020-03-21 22:50:41 +0100

    added register view

---
+++ src/components/card.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <v-card class="mx-auto" max-width="344" outlined>
+            <v-img :src="image" height="200px"></v-img>
+            <v-card-title>
+                {{ title }}
+            </v-card-title>
+            <v-card-text class="text--primary">
+                {{ text }}
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'card',
+        props: {
+            image: String, 
+            title: String, 
+            text: String
+        },
+        data: () => ({}),
+    }
+</script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,9 +1,47 @@
 <template>
   <div class="register">
       <v-container>
-        <v-row class="text-center">
-            <v-col cols="12">Registrierungsformular</v-col>
+        <v-row class="text-left">
+            <v-col cols="12"><h1>Komm in unser Team</h1></v-col>
+        </v-row>
+        <v-row class="text-left">
+            <v-col cols="8">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="4">
+              <card 
+              image="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" 
+              title="Medizinische Einrichtung"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </v-col>
+            <v-col cols="4">
+              <card 
+              image="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" 
+              title="Freiwillige Helfer"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </v-col>
+            <v-col cols="4">
+              <card 
+              image="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" 
+              title="Patienten"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              />
+            </v-col>
         </v-row>
       </v-container>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import card from '@/components/card.vue'
+
+export default {
+  name: 'Register',
+  components: {
+    card
+  }
+}
+</script>
