57fff743d3002d9eae448791ccc3e05baf42d19e
Anton Schuster <Anton.Schuster@sybit.de>
2020-03-21 21:07:58 +0100

    renamed helloworld page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld/>
+    <welcomePage/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import welcomePage from '@/components/welcomePage.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    welcomePage
   }
 }
 </script>
