d30edd40cb270c45fea0c63319a6477483d33665
Andreas Becker <mail@andreasbecker.de>
2020-03-22 18:35:57 +0100

    Text for hotline

--- src/views/Hotline.vue
+++ src/views/Hotline.vue
@@ -4,7 +4,8 @@
         <v-row class="text-left">
             <v-col cols="12">
               <h1>Weiterleitung einrichten</h1>
-              <p>Lorem ispum...</p>
+              <p>Hier folgt dann später noch eine Einführung, wie ärztliche Einrichtungen und Praxen einfach auf den digitalen medizinischen Assistenten umstellen können.</p>
+              <p>Für alle interessierten ist der digitale medizinische Assistent bereits unter nachfolgender Nummer erreichbar</p>.
               <div class="align-center d-flex body-1">
                 <span class="mdi mdi-phone-outline mdi-2 pr-2 mb-n3 display-2	"></span>
                 <span class="headline">+49 69 50985984</span>
