f1861b984a8dc015b5b4b0f50070488d1b185219
Andreas Becker <mail@andreasbecker.de>
2020-03-22 13:19:14 +0100

    optimized images, added hotline page

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,3 +1,4 @@
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
@@ -5,6 +6,9 @@ import 'vuetify/dist/vuetify.min.css';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdiSvg',
+    },
     theme: {
         themes: {
             light: {
